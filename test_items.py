9b3c89898f4a2be83c7fb4d0edf887750802c00f
Пользователь <eightshaman@gmail.com>
2021-11-30 13:56:47 +0300

    поправил assert

--- test_items.py
+++ test_items.py
@@ -4,8 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_to_basket_on_page(browser):
     browser.get(link)
-    time.sleep(30)
-    try:
-        button_add_to_basket = browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
-    except:
-        assert button_add_to_basket.is_displayed() == True, "Кнопка отсутствует"
+    time.sleep(5)
+    button_add_to_basket = browser.find_elements_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert len(button_add_to_basket) == 1, "Кнопка отсутствует"
